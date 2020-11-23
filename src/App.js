928a92a5b42c917fc9bfae913f7a0e4fe8ff4b02
Diana <idiana96@yahoo.com>
2020-11-23 14:34:29 +0000

    ready for firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
-
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
+      </Switch>
     </div>
   );
 }
